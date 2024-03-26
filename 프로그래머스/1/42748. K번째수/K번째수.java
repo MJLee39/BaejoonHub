102a3a95833b61ba390e1fd286ef88b409af34f9
MJLee39 <115640392+MJLee39@users.noreply.github.com>
2024-03-26 09:41:42 +0900

    [level 1] Title: K번째수, Time: 11.89 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0;i<commands.length;i++){
+            int num=0;
+            int[] temp = new int[commands[i][1]-commands[i][0]+1];
+            for(int j=commands[i][0]-1; j<=commands[i][1]-1; j++){
+                temp[num] = array[j];
+                System.out.print("temp["+num+"]:"+temp[num]+",");
+                num++;
+            }
+            Arrays.sort(temp);
+            System.out.print(Arrays.toString(temp));
+            answer[i]=temp[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
