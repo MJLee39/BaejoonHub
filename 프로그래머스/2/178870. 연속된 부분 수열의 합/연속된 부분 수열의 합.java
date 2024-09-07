83a5f901cbb347736e5c28c24952eca130d1710b
MJLee39 <115640392+MJLee39@users.noreply.github.com>
2024-09-07 19:16:08 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 22.73 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -8,30 +8,31 @@ class Solution {
         int end = 0;
         int min = 1000000000;
         int sum = sequence[start];
+        int len = sequence.length;
         
-        for(int i=0; i<sequence.length; i++){
-            while(end < sequence.length){
-                if(sum == k){
-                    if((end-start) < min){
-                        min = end - start;
-                        answer[0] = start;
-                        answer[1] = end;
-                    }
-                    end++;
-                    if(end < sequence.length){
-                        sum += sequence[end];
-                    }
-                }else if(sum < k){
-                    end++;
-                    if(end < sequence.length){
-                        sum += sequence[end];
-                    }
-                }else{
-                    sum -= sequence[start];
-                    start++;
+        //앞에서부터 시작하므로 길이가 짧은 수열이 여러개인 경우 앞쪽에 나오는 수 만족
+        while(end < len){
+            if(sum == k){  //같으면 최적인지 확인
+                if((end-start) < min){
+                    min = end - start;
+                    answer[0] = start;
+                    answer[1] = end;
                 }
+                end++;
+                if(end < len){
+                    sum += sequence[end];
+                }
+            }else if(sum < k){
+                end++;
+                if(end < len){
+                    sum += sequence[end];
+                }
+            }else{
+                sum -= sequence[start];
+                start++;
             }
-        }   
+        }
+        
         
         return answer;
     }
