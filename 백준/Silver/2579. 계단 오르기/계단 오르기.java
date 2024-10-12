0c1cb7580253737edf05f714caee7f005eb49a20
MJLee39 <115640392+MJLee39@users.noreply.github.com>
2024-10-12 10:53:00 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,48 +1,38 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	     StringTokenizer st = new StringTokenizer(br.readLine());
-	     StringBuilder sb = new StringBuilder();
-	     
-	     int n = Integer.parseInt(st.nextToken());
-	     
-	     int[] step = new int[n];
-	     
-	     for(int i=0; i<n; i++) {
-	    	 st = new StringTokenizer(br.readLine());
-	    	 step[i] = Integer.parseInt(st.nextToken());
-	     }
-	     
-	     int[] score = new int[n];
-	     
-	     
-	     score[0] = step[0];  //첫번째칸
-	     if(n>1) {
-	    	 score[1] = step[0]+step[1];  //두번째칸
-	     }
-	     
-	     if(n>2) {
-	    	 score[2] = Math.max(step[0]+step[2], step[1]+step[2]);  //세번째칸
-	     }
-	     
-	     if(n>2) {
-		     for(int i=3; i<n; i++) {
-		    	 score[i] = Math.max(score[i-2], score[i-3]+step[i-1])+step[i];  //i+1번째 칸
-		     }
-	     }
-	     sb.append(score[n-1]);
-	     bw.write(sb.toString());
-	     bw.flush();
-	     bw.close();
+	public static int dp(int n, int[] memo, int[] stair) {
+		
+		memo[1] = stair[1];
+		
+		if(n >= 2) {
+			memo[2] = Math.max(memo[1]+stair[2], stair[2]);
+		}
+		
+		if(n >= 3 && memo[n] == 0) {
+			memo[n] = Math.max(dp(n-2,memo,stair)+stair[n], dp(n-3,memo,stair)+stair[n-1]+stair[n]);
+		}
+		
+		return memo[n];
 	}
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int[] memo = new int[n+1];
+		int[] stair = new int[n+1];
+		
+		for(int i=1; i<n+1; i++) {
+			st = new StringTokenizer(br.readLine());
+			stair[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(dp(n, memo, stair));
+	}
+	
+
 }
