2ba238c67627862cc2d61585be387695507f50ed
MJLee39 <115640392+MJLee39@users.noreply.github.com>
2024-10-15 21:11:09 +0900

    [level 2] Title: 프로세스, Time: 5.26 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,30 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        Queue<Integer> q = new LinkedList<Integer>();
-        for(int i=0;i<priorities.length;i++){
-            q.offer(priorities[i]);
+        Queue queue = new LinkedList<Integer>();
+        Queue queueIndex = new LinkedList<Integer>();
+        int order = 0;
+        
+        for(int i=0; i<priorities.length; i++){
+            queue.add(priorities[i]);
+            queueIndex.add(i);
         }
-        int currentLoca = location;
-        while(!q.isEmpty()){
-            //System.out.println("max: "+Collections.max(q));
-            if(q.peek()==Collections.max(q)){
-                answer++;
-                if(currentLoca!=0){
-                    currentLoca = currentLoca-1;
-                }else{
+        
+        while(!queue.isEmpty()){
+            if(queue.peek() != Collections.max(queue)){
+                int peek = (int)queue.remove();
+                queue.add(peek);
+                int peekIndex = (int)queueIndex.remove();
+                queueIndex.add(peekIndex);
+            }else{
+                int out = (int)queue.remove();
+                int outIndex = (int)queueIndex.remove();
+                order++;
+                if(outIndex == location){
+                    answer = order;
                     break;
                 }
-                q.poll();
-                //System.out.println(q);
-            }else{
-                q.offer(q.poll());
-                if(currentLoca==0){
-                    currentLoca = q.size()-1;
-                }else{
-                    currentLoca = currentLoca-1;
-                }
-                //System.out.println(q);
             }
         }
         
